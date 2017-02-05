caeb3d16c1dd17e1f606f3b316a2000b32b74938
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-02-06 00:29:41 +0100

    feature(user model): Create user model file

---
+++ server/app/models/user.js
@@ -0,0 +1,59 @@
+import bcrypt from 'bcrypt-nodejs';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    roleId: {
+      type: DataTypes.INTEGER,
+      defaultValue: 2
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      generateHash() {
+        this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+      },
+      validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    },
+    hooks: {
+      beforeCreate(user) {
+        user.generateHash();
+      },
+      beforeUpdate(user) {
+        if (user._changed.password) {
+          user.generateHash();
+        }
+      }
+    }
+  });
+  return User;
+};
