3f22f987295d566c2927a3eeaad712200218be40
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-02-10 00:16:55 +0100

    Update(app): Update app with roles route

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import userRouter from './routes/userRoute';
 import docRouter from './routes/documentRoute';
+import roleRouter from './routes/roleRoute';
 
 // Set up the express app
 const app = express();
@@ -14,7 +15,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Document Management System' });
+});
+
 app.use('/users', userRouter);
 app.use('/documents', docRouter);
+app.use('/roles', roleRouter);
 
 export default app;
