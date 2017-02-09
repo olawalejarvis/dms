7c87240dd7ae2d0d8c37e2af9cc5b846f8eba9ca
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-02-10 00:19:24 +0100

    Refactor(server): Make the console link more readable

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
-console.log('Running on port ', port);
+console.log(`App is running at: ${process.env.HOST}:${port}/`);
