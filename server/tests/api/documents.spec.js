e7ce65a5a53535c92eb07ca3e2c9cf86011e79f4
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-02-11 23:35:08 +0100

    feature(test): Add more test cases to documents spec

--- server/tests/api/documents.spec.js
+++ server/tests/api/documents.spec.js
@@ -21,7 +21,7 @@ const regularRoleParams = helper.testRoleR;
 const adminRoleParams = helper.testRoleA;
 
 let adminToken;
-let regulartoken;
+let regularToken;
 let regularToken2;
 
 let adminUser;
@@ -30,6 +30,8 @@ let regularUser2;
 
 let adminRole;
 let regularRole;
+
+let createdDoc;
 
 describe('DOCUMENT API', () => {
   before((done) => {
@@ -50,7 +52,7 @@ describe('DOCUMENT API', () => {
                 superRequest.post('/users')
                   .send(regularParams)
                   .end((err1, res1) => {
-                    regulartoken = res1.body.token;
+                    regularToken = res1.body.token;
                     regularUser = res1.body.user;
                     superRequest.post('/users')
                       .send(regularParams2)
@@ -75,5 +77,130 @@ describe('DOCUMENT API', () => {
     expect(regularUser2.username).to.equal(regularParams2.username);
     done();
   });
-  describe()
+  describe('CREATE POST /documents', () => {
+    after(() => {
+      db.Document.destroy({ where: {} });
+    });
+
+    it('should create a new document successfully', (done) => {
+      superRequest.post('/documents')
+        .send(publicD)
+        .set({ 'x-access-token': regularToken })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.document.title).to.equal(publicD.title);
+          expect(res.body.document.ownerId).to.equal(regularUser.id);
+          expect(res.body.document.access).to.equal(publicD.access);
+          done();
+        });
+    });
+    it('should not create document if no token is supply', (done) => {
+      superRequest.post('/documents')
+        .send(publicD)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('verification failed');
+          done();
+        });
+    });
+    it('should not create document when no title or content is supply', (done) => {
+      const invalidDoc = { content: 'new document' };
+      superRequest.post('/documents')
+        .send(invalidDoc)
+        .set({ 'x-access-token': adminToken })
+        .end((err, res) => {
+          expect(res.status).to.equal(409);
+          expect(res.body.message).to.equal('error');
+          done();
+        });
+    });
+  });
+  describe('Update Document /documents/:id', () => {
+    beforeEach((done) => {
+      superRequest.post('/documents')
+        .send(publicD)
+        .set({ 'x-access-token': regularToken })
+        .end((err, res) => {
+          createdDoc = res.body.document;
+          done();
+        });
+    });
+    it('should update doucment for owner alone', (done) => {
+      const updateDoc = { title: 'andela' };
+      superRequest.put(`/documents/${createdDoc.id}`)
+        .send(updateDoc)
+        .set({ 'x-access-token': regularToken })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.upDoc.title).to.equal(updateDoc.title);
+          expect(res.body.upDoc.content).to.equal(createdDoc.content);
+          done();
+        });
+    });
+    it('should not update document when user is not the owner', (done) => {
+      const updateDoc = { content: 'new life, new culture, new community' };
+      superRequest.put(`/documents/${createdDoc.id}`)
+        .send(updateDoc)
+        .set({ 'x-access-token': regularToken2 })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('permission denied');
+          done();
+        });
+    });
+    it('should not update document when token is not supply', (done) => {
+      const updateDoc = { content: 'new life, new culture, new community' };
+      superRequest.put(`/documents/${createdDoc.id}`)
+        .send(updateDoc)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('verification failed');
+          done();
+        });
+    });
+  });
+  describe('Delete Document DELETE /documents/:id', () => {
+    let document;
+    beforeEach((done) => {
+      superRequest.post('/documents')
+        .send(privateD)
+        .set({ 'x-access-token': regularToken2 })
+        .end((err, res) => {
+          document = res.body.document;
+          done();
+        });
+    });
+    it('should delete document from owner', (done) => {
+      superRequest.delete(`/documents/${document.id}`)
+        .set({ 'x-access-token': regularToken2 })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.message).to.equal('document deleted');
+          done();
+        });
+    });
+    it('should not delete document if not the owner', (done) => {
+      superRequest.delete(`/documents/${document.id}`)
+        .set({ 'x-access-token': regularToken })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('permission denied');
+          done();
+        });
+    });
+    it('should display not found when id is not found', (done) => {
+      superRequest.delete('/documents/100')
+        .set({ 'x-access-token': regularToken2 })
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          expect(res.body.message).to.equal('no document found');
+          done();
+        });
+    });
+  });
+  describe('Get user by id /documents/:id', () => {
+    describe('get user id with access private', () => {
+      
+    });
+  });
 });
