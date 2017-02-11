dc9b748c5d715c1aab360859dabace054a03cf15
andela-oaladeusi <olawale.aladeusi@andela.com>
2017-02-11 14:02:39 +0100

    feature(helper): Generte test helper

---
+++ server/tests/test.helper.js
@@ -0,0 +1,87 @@
+import faker from 'faker';
+
+const helper = {
+  testRoleA: {
+    id: 1,
+    title: 'admin'
+  },
+  testRoleR: {
+    id: 2,
+    title: 'regular'
+  },
+  adminUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  },
+  regularUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  },
+  firstUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+  secondUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+  thirdUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  },
+  invalidEmailUser: {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: 'kkkkk',
+    password: faker.internet.password()
+  },
+  testDocument1: {
+    title: faker.company.catchPhrase(),
+    content: faker.lorem.paragraph(),
+    access: 'public'
+  },
+  testDocument2: {
+    title: faker.company.catchPhrase(),
+    content: faker.lorem.paragraph(),
+    access: 'private'
+  },
+  testDocument3: {
+    title: faker.company.catchPhrase(),
+    content: faker.lorem.paragraph(),
+    access: 'role'
+  },
+  testDocument4: {
+    title: faker.company.catchPhrase(),
+    content: faker.lorem.paragraph(),
+  },
+  documentArray() {
+    const documentData = [];
+    for (let i = 0; i <= 15; i += 1) {
+      documentData.push({
+        title: faker.company.catchPhrase(),
+        content: faker.lorem.paragraph(),
+        OwnerId: 1
+      });
+    }
+    return documentData;
+  }
+
+};
+
+export default helper;
